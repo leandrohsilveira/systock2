c060dce2c7d3b1a15bcbff9b26e4fe8a1622aec8
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-05-31 14:06:59 -0300

    Improved input validation behavior to detect validation errors on dirty input

--- src/main/resources/src/app/widgets/field/input.jsx
+++ src/main/resources/src/app/widgets/field/input.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import Input from 'react-toolbox/lib/input';
 
-export default ({input, className, label, disabled, required, type, icon, meta: {touched, error}}) => {
+export default ({input, className, label, disabled, required, type, icon, meta: {dirty, touched, error}}) => {
     return (
-        <Input required={required} className={className} icon={icon} type={type} error={touched ? error : null} label={label} disabled={disabled} {...input}></Input>
+        <Input required={required} className={className} icon={icon} type={type} error={(dirty || touched) ? error : null} label={label} disabled={disabled} {...input}></Input>
     )
 }
